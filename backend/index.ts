2b9241f9a11aa1193cfc2341d275c8dd2f68f462
its-id <ik.dana24@gmail.com>
2024-01-03 03:14:05 +0530

    Fix BACKEND Route handling

--- backend/index.ts
+++ backend/index.ts
@@ -8,12 +8,12 @@ import userRouter from './routes/user';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = 3001;
 mongoose.connect(process.env.DB_URL as string);
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: `http://localhost:3000, ${process.env.FRONTEND_URL}}`,
     credentials: true,
   })
 );
