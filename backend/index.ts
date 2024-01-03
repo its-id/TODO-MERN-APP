51d48c35356d18a5ff9443cc42918cd877c1b594
its-id <ik.dana24@gmail.com>
2024-01-03 18:25:47 +0530

    Update Tab Title

--- backend/index.ts
+++ backend/index.ts
@@ -13,7 +13,7 @@ mongoose.connect(process.env.DB_URL as string);
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL as string],
+    origin: [process.env.FRONTEND_URL as string], //or origin: [process.env.FRONTEND_URL as string, 'http://localhost:5173/'],
     credentials: true,
   })
 );
